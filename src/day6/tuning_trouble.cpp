9b567c5672db56d27be92ddc4ba6345646162921
davidcutting <dcutting@umich.edu>
2022-12-06 13:53:57 -0500

    day6 part2

--- src/day6/tuning_trouble.cpp
+++ src/day6/tuning_trouble.cpp
@@ -35,6 +35,50 @@ auto slice_out_of_stack(const char& c, std::vector<char>& stack) noexcept -> voi
     std::swap(out_stack, stack);
 }
 
+auto marker_finder(const std::string& line, std::vector<char>& processed, uint32_t unique_char_desired = 4) noexcept -> uint32_t
+{
+    uint32_t unique_char_count = 0;
+    uint32_t start_marker = 0;
+    char last_c = 0;
+    for (auto c : line)
+    {
+        std::cout << c;
+        // Check to see if we have all the characters found
+        if (unique_char_count == unique_char_desired)
+        {
+            // End
+            break;
+        }
+        // Is this c the same as the last one we saw?
+        else if (c == last_c)
+        {
+            // Empty stack and restart.
+            processed.clear();
+            unique_char_count = 1;
+            processed.emplace_back(c);
+            start_marker++;
+            continue;
+        }
+        // Different from last one... Lets check the stack
+        else if (is_char_in_stack(c, processed))
+        {
+            slice_out_of_stack(c, processed);
+            last_c = c;
+            unique_char_count = processed.size() + 1;
+            processed.emplace_back(c);
+            start_marker++;
+        }
+        else
+        {
+            // Add this new found character to the stack
+            processed.emplace_back(c);
+            unique_char_count++;
+            start_marker++;
+        }
+    }
+    return start_marker;
+}
+
 int main(int argc, char* argv[])
 {
     // Get text from input file and make it didn't fail.
@@ -48,60 +92,37 @@ int main(int argc, char* argv[])
 
     for (auto line : text.value())
     {
-        std::vector<char> last_stack;
-        uint32_t unique_char_count = 0;
-        uint32_t start_packet_marker = 0;
-        char last_c = 0;
-        const uint32_t UNIQUE_CHAR_DESIRED = 4;
-        //std::string line = text.value()[0];
-        for (auto c : line)
-        {
-            std::cout << c;
-            // Check to see if we have all the characters found
-            if (unique_char_count == UNIQUE_CHAR_DESIRED)
-            {
-                // End
-                break;
-            }
-            // Is this c the same as the last one we saw?
-            else if (c == last_c)
-            {
-                // Empty stack and restart.
-                last_stack.clear();
-                unique_char_count = 1;
-                last_stack.emplace_back(c);
-                start_packet_marker++;
-                continue;
-            }
-            // Different from last one... Lets check the stack
-            else if (is_char_in_stack(c, last_stack))
-            {
-                slice_out_of_stack(c, last_stack);
-                last_c = c;
-                unique_char_count = last_stack.size() + 1;
-                last_stack.emplace_back(c);
-                start_packet_marker++;
-            }
-            else
-            {
-                // Add this new found character to the stack
-                last_stack.emplace_back(c);
-                unique_char_count++;
-                start_packet_marker++;
-            }
-        }
-        std::cout << std::endl;
+        // PART 1
 
-        for (auto c : last_stack)
+        std::vector<char> processed_stack;
+        uint32_t start_packet_marker = marker_finder(line, processed_stack);
+
+        /**
+        for (auto c : processed_stack)
         {
             std::cout << c;
         }
-
         std::cout << " " << start_packet_marker << std::endl;
+        */
 
         std::string processed = line.substr(0, start_packet_marker);
-        std::string message = line.substr(start_packet_marker);
+        std::string packet = line.substr(start_packet_marker);
+        //std::cout << processed << " - " << packet << std::endl;
 
+        // PART 2
+
+        processed_stack.clear();
+        uint32_t start_message_marker = marker_finder(packet, processed_stack, 14);
+
+        for (auto c : processed_stack)
+        {
+            std::cout << c;
+        }
+        uint32_t processed_chars = start_packet_marker + start_message_marker;
+        std::cout << " " << processed_chars << std::endl;
+
+        processed = packet.substr(0, start_message_marker);
+        std::string message = packet.substr(start_message_marker);
         std::cout << processed << " - " << message << std::endl;
     }
 
